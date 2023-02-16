a104fdc51d360dc3e758fe6c06916b72ad572fbc
Muskan <102637258+Fun-25@users.noreply.github.com>
2023-02-16 20:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // your code here
-var h3=document.querySelector("url");
-var nameElement=document.querySelector("name");
-var yearElement=document.querySelector("year");
-var button=document.querySelector("button");
+var form=document.querySelector("form")
+var h3=document.querySelector("h3");
+var nameElement=document.querySelector("#name");
+var yearElement=document.querySelector("#year");
+var btn=document.querySelector("#button");
 
 function submitHandle(event) {
 	event.preventDefault();
@@ -11,15 +12,39 @@ function submitHandle(event) {
 		var name=nameElement.value;
 		var year=yearElement.value;
 	if(name && year){
-			h3value+='?name='+name + '&year='+year;
+			h3value+="?name="+name + "&year="+year;
 	}
-	elseif(name && !year){
-		h3value+='?name='+name
+	else if(name && !year){
+		h3value+="?name="+name
 	}
-	elseif(!name && year){
-		h3value+='&year='+year;
+	else if(!name && year){
+		h3value+="&year="+year;
 	}
 	h3.textContent=h3value;
 }
 
 form.addEventListener('submit',submitHandle);
+/*
+var form = document.querySelector("form");
+var h3 = document.querySelector("h3");
+var nameELement = document.querySelector("#name");
+var yearElement = document.querySelector("#year");
+var btn = document.querySelector("#button");
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var h3Value = "https://localhost:8080/";
+  var name = nameELement.value;
+  var year = yearElement.value;
+
+  if (name && year) {
+    h3Value += "?name=" + name + "&year=" + year;
+  } else if (name && !year) {
+    h3Value += "?name=" + name;
+  } else if (!name && year) {
+    h3Value += "?year=" + year;
+  }
+  h3.textContent = h3Value;
+}
+
+form.addEventListener("submit", handleSubmit);*/
